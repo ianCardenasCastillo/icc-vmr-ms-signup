06758d1e9809a9d0c1b11c53675428ed89a0d626
IanCardenasCastillo <iancardenas96@gmail.com>
2023-01-17 23:51:56 -0300

    re-import modules and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
-import { EnvironmentService } from './environment/services/environment.service';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseService } from './database/database.service';
-import { EnvironmentModule } from './environment';
+import { DatabaseService } from './core/database';
+import { EnvironmentModule, EnvironmentService } from './core/environment';
 import { Tenant, TenantSchema } from './schemas/tenant.schema';
 
 @Module({
