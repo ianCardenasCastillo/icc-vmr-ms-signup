d0b155de67b498eb5f875b45780aea34bcbff21b
IanCardenasCastillo <iancardenas96@gmail.com>
2023-01-17 23:45:14 -0300

    apply mongoose config app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,31 @@
+import { EnvironmentService } from './environment/services/environment.service';
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseService } from './database/database.service';
+import { EnvironmentModule } from './environment';
+import { Tenant, TenantSchema } from './schemas/tenant.schema';
 
 @Module({
-  imports: [],
+  imports: [
+    EnvironmentModule,
+    MongooseModule.forRootAsync({
+      imports: [EnvironmentModule],
+      useClass: DatabaseService,
+      inject: [EnvironmentService]
+    }),
+    MongooseModule.forFeature([
+      {
+        name: Tenant.name,
+        schema: TenantSchema,
+      },
+    ]),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    DatabaseService
+  ],
 })
 export class AppModule {}
