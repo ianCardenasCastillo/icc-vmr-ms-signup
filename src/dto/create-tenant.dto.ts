bbc304d93c0c0eb6c13718a9f5706f98e41496ad
IanCardenasCastillo <iancardenas96@gmail.com>
2023-01-18 20:59:53 -0300

    add validation dto

--- src/dto/create-tenant.dto.ts
+++ src/dto/create-tenant.dto.ts
@@ -1,5 +1,10 @@
+import { IsEmail, IsNotEmpty } from "class-validator";
+
 export class CreateTenantDto {
+    @IsEmail()
     email: string;
+
+    @IsNotEmpty()
     password: string
     
     static describe(instance): Array<string> {
