cc9b95f539aa0799ecc03d70789278def11e7666
IanCardenasCastillo <iancardenas96@gmail.com>
2023-01-17 22:27:08 -0300

    add schema tenant

---
+++ src/schemas/tenant.schema.ts
@@ -0,0 +1,90 @@
+import { HydratedDocument } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { StatusTenantAvailable } from '../enums/tenant.enum';
+import * as crypto from 'crypto';
+
+export type TenantDocument = HydratedDocument<Tenant>;
+@Schema({
+  autoIndex: false,
+  toJSON: { transform: omitSensitive },
+})
+export class Tenant {
+  // Datetime management
+  @Prop({
+    type: Date,
+    required: false,
+  })
+  createdAt: Date;
+  @Prop({
+    type: Date,
+    required: false,
+  })
+  updatedAt: Date;
+  @Prop({
+    type: Date,
+    required: false,
+  })
+  lastLogin: Date;
+
+  // Status management
+  @Prop({
+    type: String,
+    required: true,
+    enum: StatusTenantAvailable,
+  })
+  status: StatusTenantAvailable;
+
+  @Prop({
+    type: String,
+    required: true,
+    lowercase: true,
+    index: true,
+    unique: true,
+    trim: true,
+  })
+  email: string;
+  @Prop({
+    type: String,
+    required: false,
+    trim: true,
+  })
+  tenantId: string;
+  @Prop({
+    type: String,
+    required: true,
+    trim: true,
+  })
+  password: string;
+
+  @Prop({
+    type: String,
+    required: false,
+    trim: true,
+  })
+  domain: string;
+  setPassword: (password: string) => void;
+}
+
+export const TenantSchema = SchemaFactory.createForClass(Tenant);
+
+TenantSchema.pre('save', function (next: Function) {
+  this.createdAt = new Date();
+  this.updatedAt = new Date();
+  this.tenantId = this._id.toString();
+  this.domain = this.email.split('@')[1];
+  next();
+});
+
+TenantSchema.methods.setPassword = function (password: string) {
+  // Creating a unique salt for a particular Tenant
+  this.salt = crypto.randomBytes(16).toString('hex');
+  // Hashing Tenant's salt and password with 1000 iterations,
+  this.password = crypto
+    .pbkdf2Sync(password, this.salt, 8000, 64, 'sha512')
+    .toString('hex');
+};
+
+function omitSensitive(_doc, obj) {
+  delete obj.password;
+  return obj;
+}
