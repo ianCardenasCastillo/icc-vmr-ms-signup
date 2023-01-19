c01086772d3b7ca1a7c62aedc98d840f372554c7
IanCardenasCastillo <iancardenas96@gmail.com>
2023-01-18 21:28:19 -0300

    create signup service

---
+++ src/sign-up/sign-up.service.spec.ts
@@ -0,0 +1,26 @@
+import { getModelToken } from '@nestjs/mongoose';
+import { Test, TestingModule } from '@nestjs/testing';
+import { Tenant } from '../schemas/tenant.schema';
+import { SignUpService } from './sign-up.service';
+
+describe('SignUpService', () => {
+  let service: SignUpService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        SignUpService,
+        {
+          provide: getModelToken(Tenant.name),
+          useValue: {},
+        },
+      ],
+    }).compile();
+
+    service = module.get<SignUpService>(SignUpService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
---
+++ src/sign-up/sign-up.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateTenantDto } from 'src/dto/create-tenant.dto';
+import { TenantDocument } from '../schemas/tenant.schema';
+
+@Injectable()
+export class SignUpService {
+    constructor(
+        @InjectModel('Tenant') private tenantModel: Model<TenantDocument>
+    ) {}
+
+    async addTenant(createTenantDto: CreateTenantDto): Promise<any> {
+        const tenantRef = new this.tenantModel(createTenantDto);
+        tenantRef.setPassword(createTenantDto.password)
+        const validateError = tenantRef.validateSync();
+        if (validateError) {
+            throw validateError['errors'];
+        }
+        return tenantRef.save();
+    }
+}
