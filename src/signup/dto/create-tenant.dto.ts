de9160ee12eda6c56abb790e979e1359c9556fba
IanCardenasCastillo <iancardenas96@gmail.com>
2023-01-19 23:38:35 -0300

    add new validation and apiproperty swagger

--- src/signup/dto/create-tenant.dto.ts
+++ src/signup/dto/create-tenant.dto.ts
@@ -1,10 +1,30 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsStrongPassword } from 'class-validator';
 
 export class CreateTenantDto {
+  @ApiProperty({
+    required: true,
+    type: String,
+    example: 'me@domain.cl',
+    format: '/^[w-.]+@([w-]+.)+[w-]{2,4}$/g',
+  })
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    required: true,
+    type: String,
+    example: 'me@domain.cl',
+    format: '/^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z]).{8}$/',
+  })
   @IsNotEmpty()
+  @IsStrongPassword({
+    minLength: 8,
+    minLowercase: 1,
+    minNumbers: 1,
+    minSymbols: 1,
+    minUppercase: 1,
+  })
   password: string;
 
   static describe(instance): Array<string> {
