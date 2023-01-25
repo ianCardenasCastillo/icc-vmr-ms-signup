eac037bb4b8622cb8961011690efe44226e618e7
IanCardenasCastillo <iancardenas96@gmail.com>
2023-01-25 02:21:32 -0300

    remove responsability set password schema

--- src/signup/schemas/tenant.schema.ts
+++ src/signup/schemas/tenant.schema.ts
@@ -1,7 +1,7 @@
 import { HydratedDocument } from 'mongoose';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { StatusTenantAvailable } from '../enums/tenant.enum';
-import * as crypto from 'crypto';
+import { SignupUtils } from '../signup.utils';
 
 export type TenantDocument = HydratedDocument<Tenant>;
 @Schema({
@@ -70,15 +70,6 @@ export const TenantSchema = SchemaFactory.createForClass(Tenant);
 TenantSchema.index({ email: 1 }, { unique: true, name: 'emailUniqueKey' });
 
 TenantSchema.queue('setDefaultProperties', []);
-TenantSchema.queue('setPassword', []);
-TenantSchema.methods.setPassword = function () {
-  // Creating a unique salt for a particular Tenant
-  this.salt = crypto.randomBytes(16).toString('hex');
-  // Hashing Tenant's salt and password with 1000 iterations,
-  this.password = crypto
-    .pbkdf2Sync(this.password, this.salt, 8000, 64, 'sha512')
-    .toString('hex');
-};
 TenantSchema.methods.setDefaultProperties = function () {
   this.createdAt = new Date();
   this.updatedAt = new Date();
